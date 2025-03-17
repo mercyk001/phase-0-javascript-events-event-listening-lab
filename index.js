f5d193b01dbf3cd9038c1e3d56e8e03399edc942
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-03-17 20:22:01 +0300

    event listening

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert () {
+        alert('I was clickwd!');
+    }
+    input.addEventListener('click', clickAlert);
 }
